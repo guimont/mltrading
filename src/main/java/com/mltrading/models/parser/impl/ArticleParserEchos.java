1bbc5ce9e02c5a12b11b4441516deee473a9ade3
guimont <gmonteilhet@free.fr>
2016-03-10 08:15:38 +0100

    little changement

--- src/main/java/com/mltrading/models/parser/impl/ArticleParserEchos.java
+++ src/main/java/com/mltrading/models/parser/impl/ArticleParserEchos.java
@@ -1,15 +1,47 @@
 package com.mltrading.models.parser.impl;
 
 import com.mltrading.models.parser.ArticleParser;
+import com.mltrading.models.parser.ParserCommon;
+import com.mltrading.models.stock.CacheStockGeneral;
+import com.mltrading.models.stock.StockGeneral;
+import com.mltrading.repository.StockRepository;
+
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by gmo on 09/03/2016.
  */
 public class ArticleParserEchos implements ArticleParser {
     //http://investir.lesechos.fr/actions/actualites/action-accor,xpar,ac,fr0000120404,isin.html?page=5
-    static String base = "http://investir.lesechos.fr/cours/profil-societe-action-";
+    static String base = "http://investir.lesechos.fr/actions/actualites/action-";
     static String sep = ",";
-    static String end = ",isin.html";
+    static String end = ",isin.html?page";
 
     static String refCode = "tbody";
+
+
+    private void loader(StockRepository repository) {
+        for (StockGeneral g : CacheStockGeneral.getIsinCache().values()) {
+            int page = 0;
+
+            String url = base + CacheStockGeneral.getIsinCache().get(g.getCode()).getName().toLowerCase().replaceAll(" ", "-") + sep + g.getPlace().toLowerCase() + sep + g.getCodif().toLowerCase() + sep + g.getCode().toLowerCase() + end + page;
+
+
+            try {
+                String text;
+
+                System.out.println(url);
+                text = ParserCommon.loadUrl(new URL(url));
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("ERROR for : " + g.getName());
+            }
+        }
+    }
+
+
 }
